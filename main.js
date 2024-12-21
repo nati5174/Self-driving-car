f908ff00d8c92de9aefe97a2365c8a4df1942591
Nethanel Reta <nethanel.n4@gmail.com>
2024-12-20 21:50:23 -0500

    main js file

---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("myCanva")
+canvas.height=window.innerHeight;
+canvas.width=300;
+
+const ctx = canvas.getContext("2d");
+
+const car = new Car(100, 100, 30, 50);
+
+animate();
+
+function animate(){
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
+
+
